904048d33762deb552440aa62a1815bdc134b1b8
aabbell <abelalemneh68@gmail.com>
2025-07-19 17:15:41 +0300

    initial commit

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,8 +2,8 @@ from relationship_app.models import Author , Book , Librarian,Library
 
 
 def book_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    return Book.objects.filter(author= author)
+    return Author.objects.get(name=author_name).objects.filter(author= author)
+
 
 def book_list_in_library(library_name):
     return Library.objects.get(name=library_name).books.all()
