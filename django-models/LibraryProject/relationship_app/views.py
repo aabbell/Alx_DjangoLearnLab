f9e640d8c9d67ae4d0a1cc9396af5164ff115165
aabbell <abelalemneh68@gmail.com>
2025-07-20 19:59:30 +0300

    commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,7 +9,7 @@ from .models import Library
 def book_list(request):
     books = Book.objects.all()
     context = {'book_list':books}
-    return render (request, 'books/list_books.html' ,context)
+    return render (request, 'relationship_app/list_books.html' ,context)
 
 class library_list(DetailView):
     model = Library
