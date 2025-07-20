ed672ba18b1ef56e655bf1560d679fe7a213bc6e
aabbell <abelalemneh68@gmail.com>
2025-07-20 20:04:51 +0300

    commit

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -20,6 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/',views.book_list,name='list_books'),
-    path('library/',views.library_list,name='library_detail')
+    path("relationship_app/", include("relationship_app.urls")),
+
 ]
